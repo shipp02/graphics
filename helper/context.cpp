d6b5d13ac3fe74e8bfb99de942c2ecacd46d5ac3
shipp02 <51503822+shipp02@users.noreply.github.com>
2020-12-24 16:42:44 +0800

    Use std::function to bind to GLFWkeyfun - can now use std::bind with placeholders to create new functions.

--- helper/context.cpp
+++ helper/context.cpp
@@ -1,34 +1,48 @@
 #ifndef HELPER_CONTEXT
 #define HELPER_CONTEXT
 
-#include<stdio.h>
-#include<GL/glew.h>
-#include<GLFW/glfw3.h>
-
-
+#include "glm/fwd.hpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <glm/glm.hpp>
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
 
 namespace gl {
-GLFWwindow* mkWindowContextCurrent(unsigned int SCR_WIDTH, unsigned int SCR_HEIGHT) {
+GLFWwindow *mkWindowContextCurrent(unsigned int SCR_WIDTH,
+                                   unsigned int SCR_HEIGHT) {
 
+  if (glfwInit() != GL_TRUE) {
+    throw "Failed to initialise GLFW\n";
+  }
 
-    if (glfwInit() != GL_TRUE) {
-        throw "Failed to initialise GLFW\n"; 
-    }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  GLFWwindow *window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL);
+  glfwMakeContextCurrent(window);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL);
-    glfwMakeContextCurrent(window);
+  // Init GLEW
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    throw "Failed to initialize GLEW\n";
+  }
+  return window;
+}
 
-    // Init GLEW
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        throw "Failed to initialize GLEW\n";
-    }
-    return window;
+void func_key_call (GLFWwindow* window, std::function<void(GLFWwindow*, int, int, int, int)> key_func) {
+  glfwSetKeyCallback(window, *key_func.target<GLFWkeyfun>());
+}
+
 }
 };
 #endif
