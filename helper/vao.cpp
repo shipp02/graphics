d54fa0bbebe4eb109ef465f8bbec0cfca8c37bde
Aashay Shah <aashay.as99@gmail.com>
2021-03-05 16:30:00 +0800

    Start workon VAO objects. Not much currently

---
+++ helper/vao.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aashay shah on 4/3/2021.
+//
+
+#include "vao.h"
---
+++ helper/vao.h
@@ -0,0 +1,14 @@
+//
+// Created by Aashay shah on 4/3/2021.
+//
+
+#ifndef OPENGL_WITH_CONAN_VAO_H
+#define OPENGL_WITH_CONAN_VAO_H
+
+
+class vao {
+
+};
+
+
+#endif //OPENGL_WITH_CONAN_VAO_H
