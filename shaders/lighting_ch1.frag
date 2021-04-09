28cd72e8af7a30036c6987e63beaba9a670751eb
Shah <aashay.as99@gmail.com>
2021-04-09 23:33:17 +0800

    Spotlight works. The direction has to be negative to be towards the cube.

--- shaders/lighting_ch1.frag
+++ shaders/lighting_ch1.frag
@@ -11,8 +11,9 @@ struct Light {
     float constant;
     float linear;
     float quad;
+    float cutOff;
     vec3 Pos;
-    /* vec3 Dir; */
+    vec3 Dir;
 
     vec3 ambient;
     vec3 diffuse;
@@ -22,39 +23,44 @@ in vec3 Normal;
 in vec3 FragPos;
 in vec2 TexCoords;
 
-out vec4 FragColor;
 
-uniform vec3 lightColor;
 uniform vec3 viewPos;
 uniform Material material;
 uniform Light light;
 
 
+out vec4 FragColor;
 
 
 void main() {
     
-    vec3 ambient = vec3(texture(material.diffuse, TexCoords)) * light.ambient;
-
-    vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.Pos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse =  light.diffuse* (vec3(texture(material.diffuse, TexCoords))  * diff);
 
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
+    float theta = dot(lightDir, normalize(-light.Dir));
+    if(theta>light.cutOff) {
+        vec3 ambient = vec3(texture(material.diffuse, TexCoords)) * light.ambient;
 
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shiny);
-    vec3 specular = light.specular* (vec3(texture(material.specular, TexCoords)) * spec);
+        vec3 norm = normalize(Normal);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse =  light.diffuse* (vec3(texture(material.diffuse, TexCoords))  * diff);
 
-    float dist = length(light.Pos - FragPos);
-    float attenuat = 1.0 / (light.constant + light.linear * dist + light.quad * (dist * dist));
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);
 
-    ambient *= attenuat;
-    specular *= attenuat;
-    diffuse *= attenuat;
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shiny);
+        vec3 specular = light.specular* (vec3(texture(material.specular, TexCoords)) * spec);
+
+        float dist = length(light.Pos - FragPos);
+        float attenuat = 1.0 / (light.constant + light.linear * dist + light.quad * (dist * dist));
+
+        ambient *= attenuat;
+        specular *= attenuat;
+        diffuse *= attenuat;
 
 
-    vec3 result = (ambient+diffuse+specular);
-    FragColor = vec4(result, 1.0);
+        vec3 result = (ambient+diffuse+specular);
+        FragColor = vec4(result, 1.0);
+    } else {
+        FragColor = vec4(light.ambient * texture(material.diffuse, TexCoords).rgb, 1.0);
+    }
 }
--- two.cpp
+++ two.cpp
@@ -1,23 +1,26 @@
 #include "gl/bind_point.h"
 #include "gl/buffer.h"
 #include "gl/context.h"
-#include "glm/ext/matrix_transform.hpp"
-#include "glm/trigonometric.hpp"
 #include "gl/program.h"
 #include "gl/texture.h"
 #include "gl/utils.h"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/ext/vector_float3.hpp"
+#include "glm/trigonometric.hpp"
+#include "uv.h"
 #include "uv/loop.h"
+#include <chrono>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #include <memory>
-#include "GLFW/glfw3.h"
+#include <thread>
 
 void err_handle(int num, std::string str) {
     std::cout << "Error code:" << num << std::endl;
     std::cout << str << std::endl;
 }
 
-void SetUniformVec3(gl::program::ptr p, std::string name, glm::vec3 vector) {
+void setUniformVec3(gl::program::ptr p, std::string name, glm::vec3 vector) {
     glUniform3fv(p->matLocation(name), 1, glm::value_ptr(vector));
 }
 struct material {
@@ -52,29 +55,28 @@ int main() {
                                              "shaders/lighting_ch1.frag");
     gl::printErrors("box program");
     box->use();
-    std::cout<<"Program created"<<std::endl;
+    std::cout << "Program created" << std::endl;
 
     GLuint vao;
     gl::genAndBind(vao, glGenVertexArrays, glBindVertexArray);
-    auto buf_box = std::make_shared<gl::buffer>(
-                       vertices::textured_cube,
-                       std::vector<gl::point_type>{gl::X, gl::Y, gl::Z, 
-                                                   gl::Red, gl::Green, gl::Blue,
-                                                   gl::U, gl::V})
-                       ->with_bind_point(box, "Pos", gl::XYZ)
-                       ->with_bind_point(box, "fNormal", gl::RGB)
-                       ->with_bind_point(box, "fTexCoords", gl::UV);
-    std::cout<<"Here"<<std::endl;
+    auto buf_box =
+        std::make_shared<gl::buffer>(
+            vertices::textured_cube,
+            std::vector<gl::point_type>{gl::X, gl::Y, gl::Z, gl::Red, gl::Green,
+                                        gl::Blue, gl::U, gl::V})
+            ->with_bind_point(box, "Pos", gl::XYZ)
+            ->with_bind_point(box, "fNormal", gl::RGB)
+            ->with_bind_point(box, "fTexCoords", gl::UV);
+    std::cout << "Here" << std::endl;
     gl::printErrors("box buffers");
 
     glm::mat4 base(1.0f);
-    base = glm::translate(base, glm::vec3(0.0f, -0.7f, 2.5f));
+    base = glm::translate(base, glm::vec3(0.0f, 0.0f, 1.5f));
     auto rotate =
-        glm::rotate(base, glm::radians(45.0f), glm::vec3(1.0f, 0.1f, 0.0f));    
+        glm::rotate(base, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     auto modelMatPos = box->matLocation("model");
     glUniformMatrix4fv(modelMatPos, 1, GL_FALSE, glm::value_ptr(rotate));
     gl::printErrors("box matrixes.");
-    
 
     auto viewMatPos = box->matLocation("view");
     auto view =
@@ -88,19 +90,17 @@ int main() {
         glm::perspective(glm::radians(75.0f), 800 / (float)600, 0.1f, 10.0f);
     glUniformMatrix4fv(projMatPos, 1, GL_FALSE, glm::value_ptr(proj));
 
-
-
-    auto lightColor = glGetUniformLocation(box->get(), "lightColor");
-    glUniform3f(lightColor, 1.0f, 1.0f, 1.0f);
+    /* setUniformVec3(box, "lightColor", glm::vec3(1.0f, 1.0f, 1.0f)); */
 
     auto lightPosLoc = glGetUniformLocation(box->get(), "light.Pos");
 
     setFloat(box->get(), "light.constant", 1.0f);
-    setFloat(box->get(), "light.linear", 0.014f);
-    setFloat(box->get(), "light.quad", 0.007f);
+    setFloat(box->get(), "light.linear", 0.07f);
+    setFloat(box->get(), "light.quad", 0.014f);
+    setFloat(box->get(), "light.cutOff", glm::cos(glm::radians(75.0f)));
+    setUniformVec3(box, "light.Dir", glm::vec3(0.0f, -1.0f, -1.0f));
     //    auto Color = glGetUniformLocation(box->get(), "Color");
     //    glUniform3f(Color, 1.0f, 0.5f, 0.31f);
-
 
     // --------------- Textures -----------------//
     // Create the texture for diffuse maps
@@ -109,16 +109,15 @@ int main() {
     const auto spec_tex =
         gl::texture("models/cube_specular_map.png", GL_TEXTURE1);
     glUniform1i(box_material.diffuse, 0);
-    glUniform1i(box_material.specular, 1);    
+    glUniform1i(box_material.specular, 1);
 
-    auto eyePos = glGetUniformLocation(box->get(), "viewPos");
-    glUniform3fv(eyePos, 1, glm::value_ptr(glm::vec3(0.0f, 0.0f, 4.0f)));
+    setUniformVec3(box, "viewPos", glm::vec3(0.0f, 0.0f, 4.0f));
     gl::printErrors("before set all material matrices");
     glUniform1f(box_material.shinyP, box_material.shiny);
 
-    SetUniformVec3(box, "light.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
-    SetUniformVec3(box, "light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
-    SetUniformVec3(box, "light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+    setUniformVec3(box, "light.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
+    setUniformVec3(box, "light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
+    setUniformVec3(box, "light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
     gl::printErrors("set all material matrices");
 
     //-----------------Lights------------------//
@@ -138,7 +137,7 @@ int main() {
     gl::printErrors("box buffers");
 
     glm::mat4 light_base(1.0f);
-    glm::vec3 lightPos(1.0f, 2.0f, 1.0f);
+    glm::vec3 lightPos(0.0f, 2.0f, 1.0f);
     light_base = glm::translate(light_base, lightPos);
     //    auto rotate_light =
     //            glm::rotate(base, 3.14f / 2, glm::vec3(1.0f, 0.0f, 1.0f));
@@ -157,19 +156,7 @@ int main() {
     gl::printErrors("box matrixes.");
 
     auto last = glfwGetTime();
-    uv::loop l;
-    l.timer<glm::mat4>(
-        200,
-        [](glm::mat4 &m, uv_timer_t * /*e*/) {
-            /* lightPos = lightPos + glm::vec3(0.1f, 0.1f, 0.0f); */
-            m = glm::translate(m, glm::vec3(0.1f, 0.1f, 0.0f));
-            std::cout << "Hey guys" << std::endl;
-            /* m = new glm::mat4(x); */
-        },
-        std::make_unique<glm::mat4>(glm::mat4(light_base))
-    );
-    /* l.run(); */
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window.window)) {
 
         gl::backgroundColor(1.000f, 0.823f, 0.909f, 1.0);
 
@@ -178,14 +165,14 @@ int main() {
             //            rotate = glm::rotate(rotate,
             //            glm::radians(15.0f), glm::vec3(0.0f,
             //            0.0f, 1.0f));
-            lightPos = lightPos + glm::vec3(0.0f, 0.0f, 0.2f);
+            lightPos = lightPos + glm::vec3(0.0f, 0.0f, 0.3f);
             light_base =
-                glm::translate(light_base, glm::vec3(0.0f, 0.0f, 0.2f));
+                glm::translate(light_base, glm::vec3(0.0f, 0.0f, 0.3f));
         }
 
         box->use();
         glBindVertexArray(vao);
-        glUniform3fv(lightPosLoc, 1, glm::value_ptr(lightPos) );
+        glUniform3fv(lightPosLoc, 1, glm::value_ptr(lightPos));
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         gl::printErrors("Before light draw");
@@ -196,7 +183,7 @@ int main() {
         glDrawArrays(GL_TRIANGLES, 0, 36);
         gl::printErrors("After light draw");
 
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(window.window);
         glfwPollEvents();
     }
 }
